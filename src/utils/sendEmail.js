e9be4bfde42b98857a8dcddfb60a07f1b73f05d0
Sravan Surya <vsravansurya@gmail.com>
2025-07-04 23:06:32 +0530

    Added SES Email

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,55 @@
+const { SendEmailCommand } = require("@aws-sdk/client-ses");
+const { sesClient } = require("./sesClient.js");
+
+const createSendEmailCommand = (toAddress, fromAddress,subject,body) => {
+  return new SendEmailCommand({
+    Destination: {
+      CcAddresses: [
+      ],
+      ToAddresses: [
+        toAddress,
+      ],
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: "UTF-8",
+          Data: `<h1>${body}</h1>`,
+        },
+        Text: {
+          Charset: "UTF-8",
+          Data: "TEXT_FORMAT_BODY",
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: subject,
+      },
+    },
+    Source: fromAddress,
+    ReplyToAddresses: [
+      /* more items */
+    ],
+  });
+};
+
+const run = async (subject,body) => {
+  const sendEmailCommand = createSendEmailCommand(
+    "sravansurya2024@gmail.com",
+    "support@devmatchh.me",
+    subject,
+    body
+  );
+
+  try {
+    return await sesClient.send(sendEmailCommand);
+  } catch (caught) {
+    if (caught instanceof Error && caught.name === "MessageRejected") {
+      const messageRejectedError = caught;
+      return messageRejectedError;
+    }
+    throw caught;
+  }
+};
+
+module.exports = {run}
