fe71fe6cb961425aa495d28bfde9a3a421895c12
Sravan Surya <vsravansurya@gmail.com>
2025-08-22 21:01:15 +0530

    Implement Sockets

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,17 +1,34 @@
 const Server = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId,targetUserId)=>{
+  return crypto
+  .createHash("sha256")
+  .update([userId,targetUserId].sort().join("_"))
+  .digest("hex");
+}
 
 const initializeSocket = (httpServer) => {
   const io = Server(httpServer, {
-    cors: {
+    cors: {  
       origin: "http://localhost:5173",
       credentials: true,
     },
   });
   io.on("connection", (socket) => {
 
-    socket.on("joinChat",()=>{})
+    socket.on("joinChat",({firstName,userId,targetUserId})=>{
+      const roomId = getSecretRoomId(userId,targetUserId);
+      console.log(firstName + " joined the chat" + roomId);
 
-    socket.on("sendMessage",()=>{})
+      socket.join(roomId);
+    })
+
+    socket.on("sendMessage",({firstName,userId,targetUserId,text})=>{
+      const roomId = getSecretRoomId(userId,targetUserId);
+      console.log(firstName + " sent a message to " + roomId + ": " + text);
+      socket.to(roomId).emit("receiveMessage", {firstName,text});
+    })
 
     socket.on("disconnect",()=>{})
     
